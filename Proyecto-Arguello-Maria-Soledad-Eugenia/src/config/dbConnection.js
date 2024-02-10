de8a2af0343dd5529b224be2c1eaba296df29a58
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-02-10 20:55:36 -0300

    ..

--- Proyecto-Arguello-Maria-Soledad-Eugenia/src/config/dbConnection.js
+++ Proyecto-Arguello-Maria-Soledad-Eugenia/src/config/dbConnection.js
@@ -8,10 +8,10 @@ export class connectDB {
 
     let URL;
 
-    if(config.enviroment.persistence === 'test'){
-        URL = config.mongo.url_test;
-    }else{
+    if(config.enviroment.persistence === 'development'){
         URL = config.mongo.url;
+    }else{
+        URL = config.mongo.url_test;
     }
 
     // const URL = config.mongo.url;
